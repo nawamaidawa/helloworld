49df91e5e367d9ac0a48f5b6092d8680a0ccc0a5
Майда Хужаниязова <majdahuzaniazova@MacBook-Pro-Majda.local>
2022-08-31 22:48:45 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+# name = input('Кто создал Питон?: ')
+#
+# while name != 'Гвидо':
+#     print('Лох')
+#     name = input('Кто создал Питон?: ')
+#
+# print('В Гугле посмотрел?')
+
+
+number = 0
+
+# while number <=100:
+#     print(number)
+#     number += 1
+
+# n = int(input('Введите n: '))
+#
+# while number <= n:
+#     print(number)
+#     number +=1
+
+# number = 0
+#
+# n = int(input('Введите n: '))
+# while number <=n:
+#     if number % 2 != 0:
+#         print(number)
+#     number +=1
+
+# name = None
+#
+# while True:
+#     name = input('Кто создал Питон?: ')
+#     if name == 'Гвидо':
+#         break
+#     print('не верно')
+#
+# print('В Гугле посмотрел?')
+
+
+# number = 0
+#
+# n = int(input('Введите n: '))
+#
+# while number <=n:
+#     if number % 2 == 0:
+#         number += 1
+#         continue
+#     print(number)
+#     number += 1
+
+number = 0
+while number <= 100:
+    print(number)
+    number +=1
+else:
+    print('end')
+
